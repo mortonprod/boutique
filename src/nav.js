2a2ecdadcb0f7a4a5be31ce4b31adc156235f80c
Alexander Morton <mortonprod@gmail.com>
2017-06-26 11:06:44 +0100

    Add auth0 and fix login style

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,8 @@
 import {Link} from 'react-router-dom'
 
 import * as _ from "lodash";
-
+import auth0 from 'auth0-js';
+import createHistory from 'history/createBrowserHistory'
 import menu from './assets/menu.svg';
 import googleC from './assets/Google_Color.svg';
 import facebookC from './assets/Facebook_Color.svg';
@@ -92,27 +93,11 @@ export default class Nav extends Component{
                       <article>
 	                      <h3> Login: </h3>
 	                      <ul className="nav__box__links__social">
-	                        <li>
-	                            <a href="https://www.facebook.com/">
-	                                <img src={facebookC} className="" alt="logo" />
-	                            </a>
-	                        </li>
-	                        <li>
-	                            <a href="https://twitter.com/?lang=en">
-	                                <img src={twitterC} className="" alt="logo" />
-	                            </a>
-	                        </li>
-	                        <li>
-	                            <a href="/https://groups.google.com/forum/#!overview">
-	                                <img src={googleC} className="" alt="logo" />
-	                            </a>
-	                        </li>
-                            <li>
-                                <Link to="/account">
-                                    <img src={accountBox} className="" alt="logo" />
-                                </Link>
+                            <li onClick={auth.login}>
+	                            <img src={accountBox} className="" alt="logo" />
+	                            <img src={facebookC} className="" alt="logo" />
+	                            <img src={googleC} className="" alt="logo" />
                             </li>
-
 	                      </ul>
                         </article>
                   </section>
@@ -127,3 +112,68 @@ export default class Nav extends Component{
         )
     }
 }
+
+
+let history = createHistory({
+  forceRefresh: true
+});
+
+class Auth {
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+  }
+  auth0 = new auth0.WebAuth({
+    domain: 'mortonprod.eu.auth0.com',
+    clientID: 'i92K1aH4gGRkkJagDPAAIjH0xmCX4A8S',
+    redirectUri: 'http://localhost:3000',
+    audience: 'https://mortonprod.eu.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login() {
+    this.auth0.authorize();
+  }
+
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace('/home');
+      } else if (err) {
+        history.replace('/home');
+        console.log(err);
+      }
+    });
+  }
+
+  setSession(authResult) {
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the 
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
+
+const auth = new Auth();
