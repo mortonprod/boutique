71bf939d2c75e1337243c5101084a714a260f11b
Alexander Morton <mortonprod@gmail.com>
2017-07-03 12:46:45 +0100

    Add product services -------------------
    Can be added to any file and will get products if not already stored.
    Can post product and category change.
    Can update product.
    Can force product update if needed on call.

---
+++ src/productsService.js
@@ -0,0 +1,62 @@
+﻿import axios from "axios";
+
+let productsServices = function(isCallAgain){ 
+    categoriesStore = null;
+    productsStore = null;
+	function init(){ 
+        axios.get('/categories').then((res) => {
+		    let categories = res.data;
+		    let catToProducts = categories.map((cat)=> {
+		        axios.get("/products/" + cat).then((res) => {
+	                return new Promise((resolve,reject) =>{resolve(res.data)});
+		        });
+		    });
+	        return new Promise.all(catToProducts).then((data) =>{
+	            let categories = null;
+	            let products = null;
+	            categories = data;
+	            data.map((el)=>{
+	                el.products.map((product)=>{
+	                    products.push(product);
+	                });
+	            });
+	            return new Promise((resolve,reject)=>{
+	                categoriesStore = categories;
+	                productsStore = products; 
+	                resolve({categories:categories,products:products});
+	            })
+            });
+        }
+    }
+    post(api,form,content){
+        if(content){
+	        axios.post(api,form,content)
+	        .then((res) => {
+	            return Promise((resolve,reject)=>{resolve(res.data)});
+	            console.log(res.data);
+	        }).catch((res)=>{
+	            return Promise((resolve,reject)=>{resolve("Error connecting to server")});
+	        });
+        }else{
+	        axios.post(api,form)
+	        .then((res) => {
+	            return Promise((resolve,reject)=>{resolve(res.data)});
+	            console.log(res.data);
+	        })
+	        .catch(function (error) {
+                return Promise((resolve,reject)=>{resolve("Error connecting to server")})
+	            console.log(error);
+	        });
+        }
+    }
+    getProducts(){
+	    if(!categoriesStore || !productsStore || isCallAgain){
+	        return init()
+	    }else{
+	        return new Promise((resolve,reject) => {resolve({categories:categoriesStore,products:productsStore})})
+	    }
+    }
+};
+
+export default productsServices;
+
