0180ee99c668246110e948262e376455ccb53f41
Alexander Morton <mortonprod@gmail.com>
2017-07-04 14:41:19 +0100

    Add password update

--- src/AdminLogin.js
+++ src/AdminLogin.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; 
 import "./AdminLogin.css";
 
 export default class AdminLogin extends Component {
     constructor(){
         super();
-        this.state = {password:null}
+        this.state = {password:null,passwordUpdate:null,isUpdate:false}
     }
-    onChange(e){
+    onChangePassword(e){
         this.setState({password:e.target.value})
     }
+    onChangePasswordUpdate(e){
+        this.setState({passwordUpdate:e.target.value})
+    }
     render(){
+        let postLoc = null;
+        let inputUpdate = null;
+        if(this.state.isUpdate){
+            postLoc = "admin-update";
+            inputUpdate = (
+                <input key={"update"} required type="password" name="passwordUpdate" value={this.state.passwordUpdate} onChange={this.onChangePasswordUpdate.bind(this)}/>
+            )
+        }else{
+            postLoc = "admin-login";
+        }
 		return (
 			<section className={"adminLogin"}>
-                <form className={"adminLogin__form"} post="admin-login" encType="application/x-www-form-urlencoded">
+                <form className={"adminLogin__form"} post={postLoc} encType="application/x-www-form-urlencoded">
                     <h1>Administration Login</h1>
 			        <h2>Password</h2>
-			        <input required type="password" name="password" value={this.state.password} onChange={this.onChange.bind(this)}/>
+			        <input required type="password" name="password" value={this.state.password} onChange={this.onChangePassword.bind(this)}/>
+                    <ReactCSSTransitionGroup
+                    transitionName="tran"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}>
+                    {inputUpdate}
+                    </ReactCSSTransitionGroup>
 			        <button type="submit">Submit Password</button>
 			    </form>
+                <button onClick={()=>{this.setState({isUpdate:!this.state.isUpdate});}}>Change Password</button>
 			</section>
 		);
     }
